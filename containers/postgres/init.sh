3b4a7fcc8e0165be52b17a86913bdcee06a02613
Jonas Colmsjö <jonas.colmsjo@gizur.com>
2013-10-01 18:44:27 +0200

    Fix

--- containers/postgres/init.sh
+++ containers/postgres/init.sh
@@ -2,7 +2,7 @@
 
 /bin/su postgres -c "/usr/lib/postgresql/9.1/bin/postgres -D /var/lib/postgresql/9.1/main -c config_file=/etc/postgresql/9.1/main/postgresql.conf" &
 sleep 5
-echo "CREATE ROLE openerp WITH SUPERUSER CREATEDB LOGIN PASSWORD 'openerp';SELECT rolname FROM pg_roles;\q"|/bin/su postgres -c "psql -U postgres"
+echo "CREATE ROLE openerp WITH SUPERUSER CREATEDB LOGIN PASSWORD 'openerp';create database openerp with owner=openerp;SELECT rolname FROM pg_roles;\q"|/bin/su postgres -c "psql -U postgres"
 
 /bin/su postgres -c "/usr/sbin/service postgresql stop"
 sleep 5
